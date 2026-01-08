1b8aef9167179764803491fd8ebe87599389b18d
Charanbalaji-Sparkout <charanmohan0508@gmail.com>
2026-01-08 17:52:20 +0530

    Textfield animation added

--- lib/screens/home/UI/home_screen.dart
+++ lib/screens/home/UI/home_screen.dart
@@ -12,7 +12,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _formkey = GlobalKey<FormState>();
+  final _datakey = GlobalKey<FormState>();
   bool _showDetails = false;
+  bool _checkboxValue = false;
 
   String? email, password, phone;
 
@@ -21,83 +23,121 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Form Validation')),
       body: _Uiwidget(),
+      backgroundColor: const Color.fromARGB(255, 212, 212, 212),
     );
   }
 
   Widget _Uiwidget() {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsetsGeometry.all(10),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: [
-              CustomFormWidget(
-                hintText: "Email",
-                validator: (val) {
-                  if (!val!.isValidEmail) {
-                    return 'Enter a Valid Email';
-                  }
-                  return null;
-                },
-                onSaved: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
-              ),
-              CustomFormWidget(
-                hintText: "Phone",
-                obsecureText: false,
-                validator: (val) {
-                  if (!val!.isValidPhone) {
-                    return 'Enter a Valid Phone Number';
-                  }
-                  return null;
-                },
-                onSaved: (val) {
-                  setState(() {
-                    phone = val;
-                  });
-                },
-              ),
-              CustomFormWidget(
-                hintText: "Password",
-                obsecureText: true,
-                validator: (val) {
-                  if (val == null || !val.isValidPassword) {
-                    return 'Enter a Valid Password';
-                  }
-                  return null;
-                },
-                onSaved: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (_formkey.currentState!.validate()) {
-                      _formkey.currentState!.save();
-                      if (email != null && password != null && phone != null) {
-                        _showDetails = !_showDetails;
-                      }
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(10),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomFormWidget(
+                  validator: (val) {
+                    if (!val!.isValidEmail) {
+                      return 'Enter a Valid Email';
                     }
-                  });
-                },
-                child: Text('Register'),
-              ),
-              if (_showDetails &&
-                  email != null &&
-                  password != null &&
-                  phone != null) ...[
-                Text("Your email is ${email ?? ''}"),
-                Text("Your number is ${phone ?? ''}"),
-                Text("Your password is ${password ?? ''}"),
+                    return null;
+                  },
+                  onSaved: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                  imgUrl:
+                      'https://i.pinimg.com/736x/dd/93/ad/dd93ad32b1b9192ced8b8fb02b61e076.jpg',
+                  labelText: 'Email',
+                ),
+                CustomFormWidget(
+                  obsecureText: false,
+                  validator: (val) {
+                    if (!val!.isValidPhone) {
+                      return 'Enter a Valid Phone Number';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    setState(() {
+                      phone = val;
+                    });
+                  },
+                  imgUrl:
+                      'https://i.pinimg.com/736x/c4/14/c7/c414c7a185dbabd361ef78b84aa2bfee.jpg',
+                  labelText: 'Phone',
+                ),
+                CustomFormWidget(
+                  obsecureText: true,
+                  validator: (val) {
+                    if (val == null || !val.isValidPassword) {
+                      return 'Enter a Valid Password';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                  imgUrl:
+                      'https://i.pinimg.com/736x/c7/5b/f0/c75bf058bd3de7435aa0626637a62df5.jpg',
+                  labelText: 'Password',
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Checkbox(
+                        value: _checkboxValue,
+                        onChanged: (val) {
+                          setState(() {
+                            _checkboxValue = val ?? false;
+                          });
+                        },
+                      ),
+                    ),
+                    Text("I agree to the Terms and Conditions"),
+                  ],
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (_checkboxValue){
+                        if (_formkey.currentState!.validate()) {
+                          _formkey.currentState!.save();
+                          if (email != null &&
+                              password != null &&
+                              phone != null) {
+                            _showDetails = !_showDetails;
+                          }
+                        }}
+                      });
+                    },
+                    child: Text('Register'),
+                  ),
+                ),
+                SizedBox(height: 40),
+                if (_showDetails &&
+                    email != null &&
+                    password != null &&
+                    phone != null) ...[
+                  Center(
+                    child: Column(
+                      children: [
+                        Text("Your email is ${email ?? ''}", key: _datakey),
+                        Text("Your number is ${phone ?? ''}"),
+                        Text("Your password is ${password ?? ''}"),
+                      ],
+                    ),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
--- lib/screens/home/widgets/custom_form_widget.dart
+++ lib/screens/home/widgets/custom_form_widget.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 
 class CustomFormWidget extends StatefulWidget {
   final bool obsecureText;
-  final String hintText;
+  final String labelText;
   final String? Function(String?) validator;
   final Function(String?) onSaved;
+  final String imgUrl;
 
   const CustomFormWidget({
     super.key,
     this.obsecureText = false,
-    required this.hintText,
     required this.validator,
     required this.onSaved,
+    required this.imgUrl,
+    required this.labelText,
   });
 
   @override
@@ -29,22 +31,86 @@ class _CustomFormWidgetState extends State<CustomFormWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsetsGeometry.all(10),
-      child: TextFormField(
-        obscureText: _isObscured,
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          suffixIcon: widget.obsecureText? IconButton(
-            onPressed: () {
-              setState(() {
-                _isObscured = !_isObscured;
-              });
-            },
-            icon: Icon(_isObscured ? Icons.visibility : Icons.visibility_off),
-          ):null
-        ),
-        validator: widget.validator,
-        onSaved: widget.onSaved,
+      padding: EdgeInsets.all(10),
+      child: Stack(
+        children: [
+          Container(
+            height: 80,
+            width: 350,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 226, 226, 226),
+              borderRadius: BorderRadius.circular(40),
+              border: Border.all(color: Colors.white),
+            ),
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 85, right: 30),
+              child: TextFormField(
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                obscureText: _isObscured,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 184, 184, 184),
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 39, 39, 39),
+                      width: 1.5,
+                    ),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromARGB(255, 159, 96, 92),
+                    ),
+                  ),
+                  labelText: widget.labelText,
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  floatingLabelStyle: TextStyle(
+                    fontSize: 14,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  errorStyle: TextStyle(
+                    fontSize: 10
+                  ),
+                  suffixIcon: widget.obsecureText
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscured = !_isObscured;
+                            });
+                          },
+                          icon: Icon(
+                            _isObscured
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            size: 18,
+                          ),
+                        )
+                      : null,
+                ),
+                onTapOutside:(event) => FocusManager.instance.primaryFocus?.unfocus(),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: widget.validator,
+                onSaved: widget.onSaved,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5,
+            left: 5,
+            child: Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                image: DecorationImage(image: NetworkImage(widget.imgUrl)),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
